97445fbe00b087a303e05f2e5402a7b90d7e1c66
Lucas Hahn <lhahn1@me.com>
2021-06-21 20:01:11 +0000

    Automate data preprocessing

--- Makefile
+++ Makefile
@@ -6,5 +6,11 @@ help:
 install:
 	conda env update -f environment.yml
 
-preprocess:
+train-playground: preprocess
+	python ddp_train_nerf.py --config configs/playground/playground.txt
+
+preprocess: download
 	sh +x bin/preprocess_data.sh data/playground
+
+download:
+	gsutil -m cp -r gs://lucas.netdron.es/data .
---
+++ bin/preprocess_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+WORKSPACE_PATH=$1
+
+colmap feature_extractor --database_path $WORKSPACE_PATH/database.db \
+  --image_path $WORKSPACE_PATH/images \
+  --ImageReader.single_camera 1 \
+  --ImageReader.camera_model SIMPLE_RADIAL \
+  --SiftExtraction.max_image_size 5000 \
+  --SiftExtraction.estimate_affine_shape 0 \
+  --SiftExtraction.max_num_features 16384 \
+
+colmap exhaustive_matcher --database_path $WORKSPACE_PATH/database.db \
+
+mkdir -p $WORKSPACE_PATH/sparse
+
+colmap mapper \
+  --database_path $WORKSPACE_PATH/database.db \
+  --image_path $WORKSPACE_PATH/images \
+  --output_path $WORKSPACE_PATH/sparse \
+
+mkdir -p $WORKSPACE_PATH/dense
+
+colmap image_undistorter \
+  --image_path $WORKSPACE_PATH/images \
+  --input_path $WORKSPACE_PATH/sparse/0 \
+  --output_path $WORKSPACE_PATH/dense \
+  --output_type COLMAP \
+  --max_image_size 2000
+
+python data_utils/generate_poses.py $WORKSPACE_PATH
+python data_utils/data_loader.py $WORKSPACE_PATH
+rm -r $WORKSPACE_PATH/posed_images $WORKSPACE_PATH/dense $WORKSPACE_PATH/sparse $WORKSPACE_PATH/database.db
